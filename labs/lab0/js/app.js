f40cb65503340add6af79a56fd948248d3ca8e15
Travis Faas <travis.faas@gmail.com>
2023-01-12 17:07:59 -0500

    Adding  base files

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800,600);
+    background(157, 252, 246);
+
+    fill(255, 54, 144);
+    rect(40,50, 120, 12);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 10);
+}
